52fb3e2f885d7bc0e6d2d2fb8f72237ea305a0b8
Baldé Amadou <baldeuniversel@protonmail.com>
2024-02-02 20:49:25 +0100

    Update of the program file <.../pdf-tool/bin/pdf-tool>, append of the file <.../bin/test_2.pdf>

--- source/pdf-tool/bin/pdf-tool.sh
+++ source/pdf-tool/bin/pdf-tool.sh
@@ -68,7 +68,7 @@ set -uo pipefail # To exit the script if there is an error
 listOfParams=("--extract" "--input" "-i" "--from" "-f" \
     "--output" "-o" "--doc" "--help")
 
-listOfParamsAction=("--extract" "--number" "--doc" "--help")
+listOfParamsAction=("--extract" "--number" "--concat" "--doc" "--help")
 
 counterOption=0
 
@@ -92,7 +92,7 @@ do
     fi
 
     #
-    if [[ $counterOption -eq 4  ]]
+    if [[ $counterOption -eq 5  ]]
     then
         echo "~"
         echo -e "Unknown `tput setaf 3; tput bold`$1` tput sgr0 ` option \U001F9D0 ."
@@ -1366,7 +1366,7 @@ then
     fi
 
 
-    ### Number operating  -> start tag[o1]
+    ### Number operating  -> end tag[o1]
    
 
     #
@@ -1374,3 +1374,364 @@ then
 fi
 
 ### Action `number` -> end tag[n0] 
+
+
+
+
+
+### Action `concat` -> start tag[c0]
+#
+if [[ "$1" == "--concat"  ]]
+then
+
+    # Declaration variables
+    flagIsPDF=""
+    argsOption=""
+    getVersionOfPDFFile=""
+    getThePDFFile=""
+    getNbPageOfPDFFile=0
+    getTheFromOptionValue=0
+    getTheToOptionValue=0
+    getTheOutputNameFile=""
+    counterIterParam=1 # Shift the action `--number`
+    counterRequiredOption=0
+
+    declare -A tabInputOptionArgs=()
+    declare -A tabOutputOptionArgs=()
+    
+    declare -a tabRequiredOptions=( "--input" "--output" )
+    declare -a tabEncodedOptions=()
+
+   
+
+    # Save the `IFS` environmental variable
+    saveIFS=$IFS
+
+    # Change the `IFS`
+    IFS=$'\n'
+    
+    # Shift `--number` action
+    shift 1
+
+
+    # 
+    for inputParam in "$@"
+    do
+        # Check if the current parameter is `--input`, then ...
+        if [[ "$inputParam" == "--input" ]] || [[ "$inputParam" == "-i" ]]
+        then
+
+            # Process to verify if the current option has already been taken, then ...
+            if [[ $counterRequiredOption -gt 0 ]]
+            then
+                # Get the content of the array in string mode
+                getInStringModeOptions=` echo ${tabEncodedOptions[@]} `
+
+                # Call the function
+                flagOptionSpotted=` isOptionRepeat "$getInStringModeOptions" "$inputParam" `
+                
+                # Checking ...
+                if [[ "$flagOptionSpotted" == "true" ]]
+                then
+                    #
+                    echo "~"
+                    echo -e "Action not performed due to repetition of \e[1;035m$inputParam\e[0m option 📛 🔰 "
+
+                    exit 1
+                fi
+            fi
+
+
+            # Get the next arguments
+            getNextArgs=` printf '"%s" ' "${@:$counterIterParam}" `
+
+            # Call of function, then ...
+            tabInputOptionArgs["--input"]=` getArgsOption  "$getNextArgs" "$inputParam" `
+
+            
+            # For the `--input` option with the `--concat` action, at least one argument
+            if [[  ` echo "${tabInputOptionArgs[@]}" | awk -F " : " '{ print NF }' ` -lt 2 ]]
+            then
+                echo "~"
+                echo -e "For the \e[1;36m--concat\e[0m action , the \e[1;035m--input\e[0m option has to have at least two arguments 🧐 "
+
+                exit 1
+            fi
+            
+            # Save `IFS`
+            saveIFSInInputOpt="$IFS"
+            IFS=" : "
+
+            # Iterate to check if the arguments of the `--input` option are pdf files
+            for inputArg in ${tabInputOptionArgs[@]}
+            do
+                # Get the input 
+                getPDFFile=` echo "$inputArg" | awk '{ gsub(/^["]+|["]+$/, ""); print }' ` 
+
+                # Check if the given input exists
+                if [[ -e "$getPDFFile" ]] && [[ ! ( -d "$getPDFFile" ) ]]
+                then
+                    # Call the functions 
+                    flagIsPDF=` isPDFFile "$getPDFFile" `
+                    
+                    # Check if the file is a pdf file, then ...
+                    if [[ "$flagIsPDF" == "true" ]]
+                    then
+                        # Call the functions
+                        getNbPageOfPDFFile=` getNbPagePDFFile $getPDFFile `
+                        getVersionOfPDFFile=` getVersionPDFFile $getPDFFile `
+    
+                        #
+                        getThePDFFile="$getPDFFile"
+    
+                    elif [[ "$flagIsPDF" == "false" ]]
+                    then
+                        echo "~"
+                        echo -e "This \e[1;031m$getPDFFile\e[0m file does not a pdf file 🧐 "
+    
+                        exit 1
+                    fi
+    
+                elif [[ -d "$getPDFFile" ]] && [[ -e "$getPDFFile" ]]
+                then
+                    #
+                    echo "~"
+                    echo -e "The argument for the \e[1;035m--option\e[0m has to be a pdf file 🧐 "
+    
+                    exit 1
+    
+                elif [[ ! ( -e "$getPDFFile" ) ]]
+                then
+                    #
+                    echo "~"
+                    echo -e "The given pdf file \e[1;031m$getPDFFile\e[0m does not exist 🧐 "
+    
+                    exit 1
+                else
+                    #
+                    echo "~"
+                    echo -e "Either the encoded file \e[1;031m$getPDFFile\e[0m does not exist or the way the"
+                    echo -e "file name is written is not supported 📛 "
+    
+                    exit 1
+                fi
+
+            done 
+
+            # Restore `IFS`
+            IFS=$saveIFSInInputOpt
+
+            # Increment the counter
+            counterRequiredOption=$(( counterRequiredOption + 1 ))
+
+            # Append the current option 
+            tabEncodedOptions+="--input "
+        
+        # Check if the current parameter is `--output`, then ...
+        elif [[ "$inputParam" == "--output" ]] ||  [[ "$inputParam" == "-o" ]]
+        then
+
+            # Process to verify if the current option has already been taken, then ...
+            if [[ $counterRequiredOption -gt 0 ]]
+            then
+                # Get the content of the array in string mode
+                getInStringModeOptions=` echo ${tabEncodedOptions[@]} `
+
+                # Call the function
+                flagOptionSpotted=` isOptionRepeat "$getInStringModeOptions" "$inputParam" `
+                
+                # Checking ...
+                if [[ "$flagOptionSpotted" == "true" ]]
+                then
+                    #
+                    echo "~"
+                    echo -e "Action not performed due to repetition of \e[1;035m$inputParam\e[0m option 📛 🔰 "
+
+                    exit 1
+                fi
+            fi
+
+
+            # Get the next arguments
+            getNextArgs=` printf '"%s" ' "${@:$counterIterParam}" `
+
+            # Call of function, then ...
+            tabOutputOptionArgs["--output"]=` getArgsOption  "$getNextArgs" "$inputParam" `
+
+            # For the `--output` option with the `--concat` action, only one argument is required
+            if [[ ` echo "${tabOutputOptionArgs[@]}" | awk -F " : " '{ print NF }' ` -gt 1  ]]
+            then
+                echo "~"
+                echo -e "For the \e[1;36m--concat\e[0m action , the \e[1;035m--output\e[0m option has to have one argument 🧐 "
+
+                exit 1
+            fi
+            #
+            if [[ ` echo "${tabOutputOptionArgs[@]}" | awk -F " : " '{ print NF }' ` -lt 1  ]]
+            then
+                echo "~"
+                echo -e "For the \e[1;36m--concat\e[0m action , the \e[1;035m--output\e[0m option has to have one argument 🧐 "
+
+                exit 1
+            fi
+
+            # Get the argument for the `--output` option 
+            getArgForOutputOption=` echo "${tabOutputOptionArgs[@]}" | awk '{gsub(/"+/, ""); print }' |  awk '{gsub(/^ | $/, ""); print}' `
+            
+
+            # Check if `getArgForOutputOption` contains spaces, then ...
+            if [[ $getArgForOutputOption =~ [[:space:]] ]]
+            then
+                #
+                echo "~"
+                echo -e "The given output name \e[1;031m$getArgForOutputOption\e[0m , has not to contain spaces 🤠 "
+
+                exit 1
+            
+            # Check to see if the given name exists in the wrapper directory
+            elif [[ -e "$getArgForOutputOption" ]]
+            then
+                echo "~"
+                echo -e "The \e[1;031m$getArgForOutputOption\e[0m name already exists in the wrapper directory 🔰 "
+
+                exit 1
+            else
+                # If the end of the name is `.pdf`, then ...
+                if [[ ` echo "$getArgForOutputOption" | grep -- ".pdf$" ` ]]
+                then
+                    #
+                    getTheOutputNameFile="$getArgForOutputOption"
+                else
+                    #
+                    getTheOutputNameFile="$getArgForOutputOption.pdf"
+                fi
+            fi
+
+            # Check if the given name output file exists, then ...
+            if [[ -e "$getTheOutputNameFile" ]]
+            then
+                echo "~"
+                echo -e "The \e[1;031m$getTheOutputNameFile\e[0m name already exists in the wrapper directory 🔰 "
+
+                exit 1
+            fi
+
+
+            # Increment the counter
+            counterRequiredOption=$(( counterRequiredOption + 1 ))
+
+            # Append the current option 
+            tabEncodedOptions+="--output "
+
+        elif ( [[ "$inputParam" != "--input" ]] || [[ "$inputParam" != "-i" ]] ) \
+            && ( [[ "$inputParam" != "--output" ]] || [[ "$inputParam" != "-o" ]] )
+        then
+            
+            #
+            if [[ ` echo "$inputParam" | grep -- "^-" ` ]]
+            then
+                echo "~"
+                echo -e "Unknown \e[1;031m$inputParam\e[0m option for the \e[1;36m--number\e[0m action 🧐 "
+            
+                exit 1
+            fi
+        fi
+
+
+        # Increment the counter 
+        counterIterParam=$(( counterIterParam + 1 ))
+    done
+
+
+    # Restore the `IFS` environmental variable  
+    IFS=$saveIFS
+    
+
+    # Check to see if the required options was used 
+    if [[ $counterRequiredOption -ne 2 ]]
+    then
+        #
+        stringMissingOptions=""
+
+        # From the table required options ...
+        for requiredOption in ${tabRequiredOptions[@]}
+        do
+            # A variable allowing to se if the current required option exists among 
+            # the input options parameters (encoded by the user)
+            flagOptionFound="false"
+
+            # From the table encoded options
+            for encodedOptions in ${tabEncodedOptions[@]}
+            do
+                # Check the matching 
+                if [[ "$requiredOption" == "$encodedOptions" ]]
+                then
+                    #
+                    flagOptionFound="true"
+                fi
+            done
+
+            # Verify if there is a match ...
+            if [[ "$flagOptionFound" == "false" ]]
+            then
+                # Append this missing option (the current required option)
+                stringMissingOptions+="$requiredOption "
+            fi
+        done
+
+        #
+        echo "~"
+        echo -e "Missing  \e[1;033m$stringMissingOptions\e[0m option(s) 🧐 "
+
+        exit 1
+    fi
+
+
+
+    ### Concatenating operating  -> start tag[o1]
+
+    if [[ -e "$tmpWorkingDir/$getThePDFFile" ]]
+    then
+        #
+        rm -r "$tmpWorkingDir/$getThePDFFile" 2> /dev/null
+    fi
+    
+
+    # Get input pdf files
+    stringPDFFiles=` echo ${tabInputOptionArgs[@]} | awk '{gsub(/ : /, " "); print }' | awk '{gsub(/["]*/, ""); print }' `
+
+    # Concatenating
+    pdftk "$stringPDFFiles" output $getTheOutputNameFile 1> /dev/null 2> "$tmpWorkingDir/concat-action" &
+
+
+    # Call the function
+    spinnerAnimation $!
+    
+
+    #
+    if [[  ` cat "$tmpWorkingDir/concat-action" 2> /dev/null | wc -l ` -eq 0  ]]
+    then
+        echo "~"
+        echo -e "Successfully extraction ✅ "
+
+        exit 0
+    else
+        echo "~"
+        echo -e "\e[1;031mError\e[0m ❌ during concatenating, make sure you have the right to write in the target"
+        echo -e "directory where the concatenated pdf file will be sent , be sure also that "
+        echo -e "your input pdf files do not contain spaces 🧐 "
+
+        exit 1
+    fi
+
+
+    ### Concatenating operating  -> end tag[o1]
+   
+
+    #
+    exit 0
+    
+fi
+
+### Action `concat` -> end tag[c0]
+
